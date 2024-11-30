058cb4b7ce47f02cf25511d92126363bf862455d
zaminalili <zaminalili29@gmail.com>
2024-11-30 22:35:13 +0400

    Add PalindromeNumber solution

--- LeetcodeSolutions/Program.cs
+++ LeetcodeSolutions/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("** Solution Testing **");
+﻿using LeetcodeSolutions.Solutions;
+
+Console.WriteLine("** Solution Testing **");
+
+int x = -121;
+int max = Convert.ToInt32(Math.Pow(2, 31) - 1);
+
+
+Console.WriteLine(PalindromeNumber.IsPalindrome(max));
---
+++ LeetcodeSolutions/Solutions/PalindromeNumber.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetcodeSolutions.Solutions;
+
+public static class PalindromeNumber
+{
+    public static bool IsPalindrome(int x)
+    {
+        string xString = x.ToString();
+
+        int i = 0;
+        int j = xString.Length - 1;
+
+        while (i < j)
+        {
+            if (xString[i] != xString[j])
+                return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
